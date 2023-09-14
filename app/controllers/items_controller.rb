12eaedeee2ebfb1b5a1b886565bc51b88f4d1b2d
Sylvia Mumbi <mumbi099@gmail.com>
2023-09-14 13:12:43 +0300

    Done lab

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,41 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    # items = Item.all
+    # render json: items, include: :user
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
+    render json: items, include: :user, status: :ok
+  end
+
+  def show
+    item = Item.find_by(id: params[:id])
+    render json: item, status: :ok
+  end
+
+  def create
+    if params[:user_id]
+      user = User.find(params[:id])
+      item = user.items.create(item_params)
+    else
+      item = Item.create(item_params)
+    end
+
+    render json: item, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
+  end
+  def render_record_not_found
+    render json: {error: "Record not found"}, status: :not_found
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
+
+  resources :items, only: [:index, :show] do
+    resources :users, only: [:index, :show, :create]
+  end
+
   resources :users, only: [:show]
+
 end
